b9b0bd00ad1f131ccb957a7f698026b966d8df97
mr-ashu <ashutosh15th@gmail.com>
2022-05-07 16:38:42 +0530

    adding body

--- index.js
+++ index.js
@@ -135,6 +135,73 @@ document.querySelector('.menuDropdown').innerHTML=
 
 
 
+
+
+
+var productContainer=document.querySelectorAll('.product-container');
+var nxtBtn=document.querySelectorAll( '.next-btn');
+var preBtn=document.querySelectorAll( '.pre-btn');
+
+
+productContainer.forEach((item,i)=>{
+    var contDimensions=item.getBoundingClientRect();
+    var contwidth=contDimensions.width;
+    nxtBtn[i].addEventListener("click",()=>{
+        item.scrollLeft +=contwidth;
+        
+    })
+    preBtn[i].addEventListener('click',()=>{
+       item.scrollLeft -=contwidth;
+    })
+    
+})
+
+
+//  sliding part------------------------------------------------
+var index=0;
+var i=0;
+
+var slider=document.querySelectorAll(".slider");
+var line=document.querySelectorAll(".line");
+auto();
+function show(n){
+    for(i=0;i<slider.length;i++){
+        slider[i].style.display="none";
+
+    }
+    for(i=0;i<line.length;i++){
+        line[i].className=line[i].className.replace("active");
+        
+    }
+    slider[n-1].style.display=("block");
+    line[n-1].className += "  active";
+}
+function auto(){
+    index++;
+    if(index>slider.length){
+        index=1;
+
+    }
+    show(index);
+    setTimeout(auto,2000)
+}
+function plusSlide(n){
+    index +=n;
+    if(index>slider.length){
+        index=1;
+    }
+    if(index<1){
+        index=slider.length;
+    }
+    show(index)
+}
+function currentSlide(n){
+    index=n;
+    show(index);
+}
+ 
+
+
 // body script end
 
 
